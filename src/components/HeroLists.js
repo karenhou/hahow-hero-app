e058fe7678bdb424d30036967e99f430c7ab6e14
Karen Hou <ih2g10@gmail.com>
2019-08-09 22:57:10 +0800

    add marvel banner to top of the herolists

--- src/components/HeroLists.js
+++ src/components/HeroLists.js
@@ -4,6 +4,14 @@ import axios from "axios";
 import HeroCards from "./HeroCards";
 import styled from "styled-components";
 
+const MarvelBanner = styled.img`
+  height: 15rem;
+
+  @media all and (max-width: 575px) {
+    height: 5rem;
+  }
+`;
+
 const HeroCardsContainer = styled(Container)`
   padding-left: 0;
   padding-right: 0;
@@ -34,6 +42,13 @@ export default class HeroLists extends Component {
   render() {
     return (
       <header>
+        <Row className="justify-content-center">
+          <MarvelBanner
+            src={require("../images/Marvel_logo.svg")}
+            alt="marvel logo"
+          />
+        </Row>
+
         <HeroCardsContainer>
           <Row className="justify-content-center">
             <HeroCards
