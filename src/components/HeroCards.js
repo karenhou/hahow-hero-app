83971a0ff9def9a2f6e6f150f07aca9488f19707
Karen Hou <ih2g10@gmail.com>
2019-08-09 22:47:35 +0800

    add images and upon select, show hero head shot

--- src/components/HeroCards.js
+++ src/components/HeroCards.js
@@ -9,18 +9,35 @@ const HeroCard = styled(Card)`
   color: #17a2b8;
 `;
 
+const HeroHeadshot = styled.img`
+  height: 16.3rem;
+
+  @media all and (max-width: 768px) {
+    height: 10rem;
+  }
+  @media all and (max-width: 575px) {
+    height: 8rem;
+  }
+`;
+
 const HeroCards = ({ heroes, current, setCurrent }) => {
   if (heroes) {
     return heroes.map(hero => {
       return (
         <Col key={hero.id} xs={3}>
           <Link to={`/${hero.id}`} onClick={() => setCurrent(hero.id)}>
-            <Card.Img variant="top" src={hero.image} alt="hero images" />
-            <HeroCard>
+            <HeroCard className="h-100">
+              {/* if hero id is same as user selected, switch images */}
+              {current === hero.id ? (
+                <HeroHeadshot
+                  src={require(`../images/${hero.name}.svg`)}
+                  alt="here images"
+                />
+              ) : (
+                <Card.Img variant="top" src={hero.image} alt="hero images" />
+              )}
               <Card.Body>
-                <Card.Title>
-                  {hero.name} {current}
-                </Card.Title>
+                <Card.Title>{hero.name}</Card.Title>
               </Card.Body>
             </HeroCard>
           </Link>
